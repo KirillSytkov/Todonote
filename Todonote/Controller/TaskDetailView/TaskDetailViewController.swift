7922f6903651e04295f820e2cfae2bc56d808572
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-04-14 22:11:04 +0300

    upadate_app

--- Todonote/Controller/TaskDetailView/TaskDetailViewController.swift
+++ Todonote/Controller/TaskDetailView/TaskDetailViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TaskDetailViewController: UIViewController {
 
     //MARK: - IBOutlets
-    
+    @IBOutlet weak var taskTitle: UITextField!
+    @IBOutlet weak var taskDate: UIDatePicker!
+    @IBOutlet weak var taskNote: UITextField!
+    @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var topView: UIView!
     
     //MARK: - vars/lets
-    
+    var item: Item?
+    let realm = try! Realm()
     
     //MARK: - lyfecycle
     override func viewDidLoad() {
@@ -23,16 +29,51 @@ class TaskDetailViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        updateUI()
     }
     
     //MARK: - IBActions
+    
     @IBAction func updateButtonPressed(_ sender: UIButton) {
+        
+        if taskTitle.text != "" {
+            let newItem = Item()
+            newItem.title = taskTitle.text ?? ""
+            newItem.date = taskDate.date
+            newItem.note = taskNote.text ?? ""
+            
+            saveTask(item: newItem)
+            dismiss(animated: true, completion: nil)
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            let alert = UIAlertController(title: "Add some title", message: "", preferredStyle: .alert)
+            present(alert, animated: true, completion: nil)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
+        
+    }
+        
+    //MARK: - flow func
+    private func updateUI() {
+        topView.layer.cornerRadius = 20
+        guard let item = item else { return }
+        updateButton.layer.cornerRadius = 10
+        taskTitle.text = item.title
+        taskNote.text = item.note
+        taskDate.date = item.date
     }
     
-    
-    //MARK: - flow func
-    
-    
-
+    private func saveTask(item: Item) {
+        do {
+            try realm.write({
+                item
+            })
+        } catch {
+            debugPrint(error)
+        }
+    }
 }
+
--- Todonote/Controller/TaskView/TasksViewController.swift
+++ Todonote/Controller/TaskView/TasksViewController.swift
@@ -16,41 +16,46 @@ class TasksViewController: UIViewController {
     @IBOutlet weak var topView: UIView!
     
     //MARK: - vars/lets
-    let realm = try! Realm()
-    private var tasks: Results<Item>?
-    
+    var categoryLabel: String?
+    private let realm = try! Realm()
+    var tasks: Results<Item>?
+    var selectedCategory: Category?
+
     //MARK: - lyfecycle
     override func viewDidLoad() {
+        super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        super.viewDidLoad()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        loadTasks()
         updateUI()
     }
     
     //MARK: - IBActions
     @IBAction func addPressed(_ sender: UIBarButtonItem) {
         guard let controller = storyboard?.instantiateViewController(withIdentifier: "AddTaskViewController") as? AddTaskViewController else { return }
-        
+        controller.selectedCategory = selectedCategory
         self.navigationController?.pushViewController(controller, animated: true)
     }
     
     
     //MARK: - flow func
     private func updateUI() {
-        topView.layer.cornerRadius = 10
+        topView.layer.cornerRadius = 20
+        categoryTitle.text = categoryLabel
     }
     
-    private func loadCategory() {
-        tasks = realm.objects(Item.self)
+    private func loadTasks() {
+//        tasks = realm.objects(Item.self)
+        tasks = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         self.tableView.reloadData()
     }
     
-    private func deleteCategory(task: Item) {
+    private func deleteTask(task: Item) {
         do {
             try realm.write({
                 realm.delete(task)
@@ -68,8 +73,26 @@ extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TasksTableViewCell", for: indexPath) as? TasksTableViewCell else { return UITableViewCell() }
-        
+        cell.configure(task: tasks![indexPath.row])
         return cell
     }
-  
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let controller = storyboard?.instantiateViewController(withIdentifier: "TaskDetailViewController") as? TaskDetailViewController else { return }
+        controller.item = tasks![indexPath.row]
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .delete
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard let task = tasks else { return }
+    
+        if editingStyle == .delete {
+            self.deleteTask(task: task[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
